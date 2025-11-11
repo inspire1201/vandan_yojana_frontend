3a19b8f04e1605172a0861f030160cd303a16347
manish <manishkeer530@gmail.com>
2025-11-11 13:27:09 +0530

    url chang 4000 to 6000

--- src/service/axiosInstance.ts
+++ src/service/axiosInstance.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://44.197.21.241:4000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://44.197.21.241:6000";
 // const API_BASE_URL = "";
 
-
+console.log("API_BASE_URL", API_BASE_URL);
 // Create an Axios instance
 const axiosInstance = axios.create({
   baseURL: `${API_BASE_URL}/api/v1`, // Set the base URL once here
